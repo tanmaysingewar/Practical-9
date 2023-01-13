8098f8052f179e2d1c59ad76386862d14896bc88
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2023-01-13 16:10:45 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Yeshwantrao Chavan College of Engineering (YCCE), Nagpur </p>
+        <img src='https://www.thehitavada.com/Encyc/2020/6/1/2_11_38_05_Yeshwantrao-Chavan-College-of-Engg-_1_H@@IGHT_439_W@@IDTH_655.jpg' />
+      <p style={{textAlign : "center",marginLeft : 200, marginRight : 200}}>Yeshwantrao Chavan College of Engineering is an autonomous engineering college affiliated to RTMNU (formerly, Nagpur University). It is located in town of Hingna in the district of Nagpur. The college was established in 1984 and is named after Yashwantrao Chavan, former first Chief Minister of Maharashtra State and the former Deputy Prime Minister of India; since then, it has been under the administration of Nagar Yuwak Shikshan Sanstha, a subsidiary of Meghe Group. It attained its autonomous status from the University Grants Commission (India) in 2010. The college offers engineering degrees at undergraduate, postgraduate and doctoral level.</p>
       </header>
     </div>
   );
